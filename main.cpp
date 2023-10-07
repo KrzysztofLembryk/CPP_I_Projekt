59a2e249421b0560c7b692511ce37ac3292146d9
KrzysztofLembryk <krzysztof.lembryk@gmail.com>
2023-10-07 17:01:04 +0200

    function that calculates if more than 10 min stay

--- main.cpp
+++ main.cpp
@@ -25,7 +25,7 @@ int manuallyReadData(string const &line, string *arr, size_t sizeOfArr)
 
     // if there are only two strings on input, we dont want to have
     // in arr[2] random values
-    for(size_t k = 0; k < sizeOfArr; k++)
+    for (size_t k = 0; k < sizeOfArr; k++)
         arr[k] = "";
 
     while (j < n && count <= 3)
@@ -78,27 +78,27 @@ int readTimePattern(string const &inputStr, string &result)
     regex timePattern("0?[0-9]\\.[0-5][0-9]|1[0-9]\\.[0-5][0-9]|2[0-3]\\.[0-5][0-9]");
     regex allowedTimePattern("0?[8-9]\\.[0-5][0-9]|1[0-9]\\.[0-5][0-9]|20\\.00");
 
-    if(inputStr.empty())
+    if (inputStr.empty())
         return 1;
-    
-    // regex won't find that 14.444 is incorrect    
-    if(inputStr.size() != 4 && inputStr.size() != 5)
+
+    // regex won't find that 14.444 is incorrect
+    if (inputStr.size() != 4 && inputStr.size() != 5)
         return -1;
 
-    // first we check whether given string has correct time format 
+    // first we check whether given string has correct time format
     regex_search(inputStr, match, timePattern);
     result = match.str();
 
     // regex_search returns "" empty string when nothing found
-    if(result.empty())
+    if (result.empty())
         return -1;
-    
+
     // if string has correct time pattern, we check whether it has time pattern
-    // allowed by our rules 
+    // allowed by our rules
     regex_search(inputStr, match, allowedTimePattern);
     result = match.str();
 
-    if (result.empty()) 
+    if (result.empty())
         return -2;
 
     return 0;
@@ -117,6 +117,49 @@ int readPlatePattern(string const &inputStr, string &result)
         return -1;
 
     return 0;
+}
+
+int convertHours(string const &time)
+{
+    //if (time[0] == '0')
+    //    return stoi(time.substr(1, 1));
+    //else
+    //{
+    if (time.size() == 5)
+        return stoi(time.substr(0, 2));
+    return stoi(time.substr(0, 1));
+    //}
+}
+
+int convertMinutes(string const &time)
+{
+    if (time.size() == 5)
+        return stoi(time.substr(3, 2));
+    return stoi(time.substr(2, 2));
+}
+
+bool calcTimeOfStay(string const &time1, string const &time2)
+{
+    int hourStart, minuteStart, hourEnd, minuteEnd;
+    int minuteDifference;
+
+    hourStart = convertHours(time1);
+    hourEnd = convertHours(time2);
+    minuteStart = convertMinutes(time1);
+    minuteEnd = convertMinutes(time2);
+
+    if (hourStart <= hourEnd)
+    {
+        minuteDifference = (hourEnd - hourStart) * 60 +
+                               minuteEnd - minuteStart;
+        return minuteDifference >= 10;
+    }
+    else
+    {
+        minuteDifference = (20 - hourStart) * 60 - minuteStart +
+                             (hourEnd - 8) * 60 + minuteEnd;
+        return minuteDifference >= 10;
+    }
 }
 
 void readLine() //(umap_t & mapp)
@@ -141,14 +184,14 @@ void readLine() //(umap_t & mapp)
                 cout << arr[i] << ", ";
 
             cout << '\n';
-            if(readPlatePattern(arr[0], plates) == -1)
+            if (readPlatePattern(arr[0], plates) == -1)
             {
                 cout << "WRONG PLATE PATTERN \n";
             }
             else
                 cout << "plate: " << plates << '\n';
-            
-            if(readTimePattern(arr[1], time1) == -1 || readTimePattern(arr[2], time2) == -1)
+
+            if (readTimePattern(arr[1], time1) == -1 || readTimePattern(arr[2], time2) == -1)
             {
                 cout << "WRONG TIME PATTERN \n";
             }
@@ -157,7 +200,6 @@ void readLine() //(umap_t & mapp)
                 cout << "time1: " << time1 << '\n';
                 cout << "time2: " << time2 << '\n';
             }
-            
         }
     }
 }
@@ -169,7 +211,7 @@ bool addToHashMap()
 
 int main()
 {
-    readLine();
-
+    //readLine();
+    cout << stoi("05");
     return 0;
 }
