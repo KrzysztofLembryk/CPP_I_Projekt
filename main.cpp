3ce47b1353993e217fc96dd75636848803b19330
KrzysztofLembryk <krzysztof.lembryk@gmail.com>
2023-10-07 15:41:11 +0200

    Added wrong input handling to ReadingTimePatterns

--- main.cpp
+++ main.cpp
@@ -15,13 +15,18 @@ namespace
     }
 }
 
-int manuallyReadData(string const &_line, string* arr, size_t sizeOfArr)
+int manuallyReadData(string const &line, string *arr, size_t sizeOfArr)
 {
     size_t i, j, count, n;
     bool foundWord = false;
-    string line = _line + " "; // temporary solution
+    // string line = _line + " "; // temporary solution
     n = line.size();
     i = j = count = 0;
+
+    // if there are only two strings on input, we dont want to have
+    // in arr[2] random values
+    for(size_t k = 0; k < sizeOfArr; k++)
+        arr[k] = "";
 
     while (j < n && count <= 3)
     {
@@ -30,16 +35,16 @@ int manuallyReadData(string const &_line, string* arr, size_t sizeOfArr)
             // skip whitespaces
             if (i == j)
                 i++;
-            else if(foundWord)
+            else if (foundWord)
             {
                 foundWord = false;
 
                 if (count < sizeOfArr)
                     arr[count] = line.substr(i, j - i);
-                
+
                 count++;
 
-                if(count > 3)
+                if (count > 3)
                     return -1;
 
                 i = j + 1;
@@ -50,21 +55,51 @@ int manuallyReadData(string const &_line, string* arr, size_t sizeOfArr)
 
         j++;
     }
+    // case when last, second/third word is present but is not followed by whitespace
+    // thus we wont go to else if(foundWord) and we wont copy this string to arr
+    if (count <= 2 && !isspace(line[j - 1]))
+    {
+        arr[count] = line.substr(i, j - i);
+        count++;
+    }
+    if (count < 2) // too few strings on input
+        return -1;
 
     return 0;
 }
 
+//  0 - inputStr has good format
+//  1 - inputStr is empty
+// -1 - inputStr is not in time format
+// -2 - inputStr is not in allowed by us time format
 int readTimePattern(string const &inputStr, string &result)
 {
-    regex timePattern("0?[8-9]\\.[0-5][0-9]|1[0-9]\\.[0-5][0-9]|20\\.00");
     smatch match;
+    regex timePattern("0?[0-9]\\.[0-5][0-9]|1[0-9]\\.[0-5][0-9]|2[0-3]\\.[0-5][0-9]");
+    regex allowedTimePattern("0?[8-9]\\.[0-5][0-9]|1[0-9]\\.[0-5][0-9]|20\\.00");
 
-    // regex_search returns "" empty string when nothing found
+    if(inputStr == "")
+        return 1;
+    
+    // regex won't find that 14.444 is incorrect    
+    if(inputStr.size() != 4 && inputStr.size() != 5)
+        return -1;
+
+    // first we check whether given string has correct time format 
     regex_search(inputStr, match, timePattern);
     result = match.str();
-    
-    if(result == "") // regex didnt find correct pattern
+
+    // regex_search returns "" empty string when nothing found
+    if(result == "")
         return -1;
+    
+    // if string has correct time pattern, we check whether it has time pattern
+    // allowed by our rules 
+    regex_search(inputStr, match, allowedTimePattern);
+    result = match.str();
+
+    if (result == "") 
+        return -2;
 
     return 0;
 }
@@ -78,7 +113,7 @@ int readPlatePattern(string const &inputStr, string &result)
 
     result = match.str();
 
-    if(result == "")
+    if (result == "")
         return -1;
 
     return 0;
@@ -94,23 +129,38 @@ void readLine() //(umap_t & mapp)
 
     getline(cin, line);
 
-    if(nonEmptyLine(line))
+    if (nonEmptyLine(line))
     {
-        manuallyReadData(line, arr, sizeArr);
-        for(int i = 0; i < 3; i++)
-            cout << arr[i] << ", ";
+        if (manuallyReadData(line, arr, sizeArr) == -1)
+        {
+            cout << "manuallyReadData - WRONG INPUT DATA \n";
+        }
+        else
+        {
+            for (int i = 0; i < 3; i++)
+                cout << arr[i] << ", ";
 
-        cout << '\n';
-        readPlatePattern(arr[0], plates);
-        cout << "plate: " << plates << '\n';
-        readTimePattern(arr[1], time1);
-        cout << "time1: " << time1 << '\n';
-        readTimePattern(arr[2], time2);
-        cout << "time2: " << time2 << '\n';
+            cout << '\n';
+            if(readPlatePattern(arr[0], plates) == -1)
+            {
+                cout << "WRONG PLATE PATTERN \n";
+            }
+            else
+                cout << "plate: " << plates << '\n';
+            
+            if(readTimePattern(arr[1], time1) == -1 || readTimePattern(arr[2], time2) == -1)
+            {
+                cout << "WRONG TIME PATTERN \n";
+            }
+            else
+            {
+                cout << "time1: " << time1 << '\n';
+                cout << "time2: " << time2 << '\n';
+            }
+            
+        }
     }
-    
 }
-
 
 bool addToHashMap()
 {
